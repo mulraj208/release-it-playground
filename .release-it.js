87eda9971b12b282b1bf8de8e420b5842a0a0bee
Mulraj Gupta <mulrajgupta208@gmail.com>
2025-07-02 12:09:50 +0530

    feat: initial commit

---
+++ .release-it.js
@@ -0,0 +1,39 @@
+module.exports = {
+  git: {
+    // eslint-disable-next-line no-template-curly-in-string
+    commitMessage: 'chore: release v${version} [ci skip]',
+    requireCleanWorkingDir: false
+  },
+  plugins: {
+    // IMPORTANT!
+    // Uncomment this plugin to use "CalVer" (yyyy.mm.dd.minor) as versioning
+    // or leave it commented to use "SemVer" (major.minor.patch)
+    // '@csmith/release-it-calver-plugin': {
+    //   // Documentation about version format:
+    //   // https://github.com/casmith/release-it-calver-plugin
+    //   // https://calver.org/
+    //   //
+    //   // NB: patch is not ignored (not working in this version)
+    //   // https://github.com/casmith/release-it-calver-plugin/issues/39
+    //   format: 'yyyy.mm.dd.minor.patch',
+    //   increment: 'calendar.minor.patch',
+    //   fallbackIncrement: 'calendar.minor'
+    // },
+    '@release-it/bumper': {
+      in: {
+        file: 'package.json',
+        path: 'version'
+      },
+      out: {
+        file: 'package.json',
+        path: 'version'
+      }
+    },
+    "@release-it/conventional-changelog": {
+      "preset": {
+        "name": "angular"
+      },
+      "infile": "CHANGELOG.md"
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+feat: add test 1 page styles
