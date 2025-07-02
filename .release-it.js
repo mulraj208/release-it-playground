8371fe75718f39e1ecb90539eaa813c705440c03
Mulraj Gupta <mulrajgupta208@gmail.com>
2025-07-02 12:20:32 +0530

    chore: release v1.1.0 [ci skip]

--- .release-it.js
+++ .release-it.js
@@ -32,7 +32,27 @@ module.exports = {
     },
     "@release-it/conventional-changelog": {
       "preset": {
-        "name": "angular"
+        "name": "conventionalcommits",
+        types: [
+          // Exampe configuration for sections generated in CHANGELOG.md
+          { type: 'feat', section: 'Features' },
+          { type: 'fix', section: 'Bug Fixes' },
+          { type: 'perf', section: 'Performance Improvements', hidden: false },
+          { type: 'refactor', section: 'Code Refactoring', hidden: true },
+          { type: 'revert', section: 'Reverts' },
+          { type: 'deprecate', section: 'Deprecations', hidden: false },
+          { type: 'remove', section: 'Removals', hidden: false },
+          { type: 'a11y', section: 'Accessibility', hidden: false },
+          { type: 'test', section: 'Tests', hidden: true },
+          { type: 'build', section: 'Build System', hidden: true },
+          { type: 'ci', section: 'Continuous Integration', hidden: true },
+          { type: 'docs', section: 'Documentation', hidden: false },
+          { type: 'style', section: 'Styles', hidden: true },
+          { type: 'change', section: 'Miscellaneous Changes', hidden: false },
+          { type: 'chore', section: 'Miscellaneous Chores', hidden: false },
+          { type: 'revert', section: 'Reverts', hidden: true },
+          { type: 'security', section: 'Security', hidden: false }
+        ]
       },
       "infile": "CHANGELOG.md"
     }
---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## 1.1.0 (2025-07-02)
+
+### Features
+
+* add test 2 page styles ([54e1a3c](https://github.com/mulraj208/release-it-playground/commit/54e1a3cf6096a2ec84ff45ba6bd59c5849f33985))
+* add test 3 page styles ([9214530](https://github.com/mulraj208/release-it-playground/commit/9214530f3aa5864f025485cb99ee6790ac90ded3))
+* initial commit ([87eda99](https://github.com/mulraj208/release-it-playground/commit/87eda9971b12b282b1bf8de8e420b5842a0a0bee))
+
+### Miscellaneous Chores
+
+* testing chore 1 ([ec2ce56](https://github.com/mulraj208/release-it-playground/commit/ec2ce56b551607dcaf6303636fc2398687673438))
+* testing chore 2 ([d5f5870](https://github.com/mulraj208/release-it-playground/commit/d5f5870752c3725edce3c2027fc938e2156547c4))
